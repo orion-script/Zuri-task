3a7a0ead4079db5faa31b1045cc4476341f6f280
Olaniyi John Ayomide <80979049+Hadestech01@users.noreply.github.com>
2021-05-21 14:51:43 +0100

    first commit

---
+++ task-4.js
@@ -0,0 +1,12 @@
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelcius").innerHTML = (valNum - 32) / 1.8;
+
+}
+
+
+function myFunction(valNum) {
+    var valNum = 'outputCelcius';
+    var outputCelcius = valNum.toFixed(4);
+    document.getElementById("outputCelcius").innerHTML = n;
+}
